171803ad203366b63c213251be22df4165a1ff8c
rkxie <amarsoft>
2018-10-30 21:29:17 +0800

    xrk:adjust async await

--- src/file.js
+++ src/file.js
@@ -3,7 +3,7 @@ import FS from "fs";
 export const readFile = () => {
     return new Promise((resolve,reject) => {
         FS.readFile(process.cwd() + '/mock/test.json',(err,data) => {
-            if (err) throw err;
+            if (err) reject(err);
             resolve(data);
         });
     });
